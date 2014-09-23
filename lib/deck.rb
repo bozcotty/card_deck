b337093698a94b0f7732f1633f2a382dd741167d
Scott Hale <shalemont@gmail.com>
2014-09-23 15:30:51 -0700

    fix spec helper, variables

--- lib/deck.rb
+++ lib/deck.rb
@@ -1,14 +1,23 @@
+require_relative 'card'
+
 class Deck
 
-  attr_accessor :count, :card_array
+  attr_reader :card_array
 
   def initialize
-    @card_array = ["ace of", "king of", "queen of", "jack of", "10 of", "9 of", "8 of", "7 of", "6 of",
+    @card_array = []
+
+    combinations = ["ace of", "king of", "queen of", "jack of", "10 of", "9 of", "8 of", "7 of", "6 of",
    "5 of", "4 of", "3 of", "2 of"].product(["clubs", "spades", "diamonds", "hearts"])
+
+    combinations.each do |c|
+      @card_array << Card.new(c[0], c[1])
+    end
   end
 
-  def card_deck
-
-  end
 
 end
+
+
+
+
